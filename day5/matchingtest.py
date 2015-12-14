a8ac5de0f2965db12502dea2a6149896f91446e1
dtext <textores.danny@gmail.com>
2015-12-14 21:53:10 +0100

    Day 5.2

--- day5/matchingtest.py
+++ day5/matchingtest.py
@@ -1,20 +1,25 @@
-__author__ = 'dt'
-
 import unittest
-import script
+import script as s
 
 
 class MyTestCase(unittest.TestCase):
 
     def test_nice(self):
-        self.assertTrue(script.matchesrules("ugknbfddgicrmopn"))
-        self.assertTrue(script.matchesrules("aaa"))
+        self.assertTrue(s.matchesrules("ugknbfddgicrmopn", s.patterns1))
+        self.assertTrue(s.matchesrules("aaa", s.patterns1))
 
     def test_naughty(self):
-        self.assertFalse(script.matchesrules("jchzalrnumimnmhp"))
-        self.assertFalse(script.matchesrules("haegwjzuvuyypxyu"))
-        self.assertFalse(script.matchesrules("dvszwmarrgswjxmb"))
+        self.assertFalse(s.matchesrules("jchzalrnumimnmhp", s.patterns1))
+        self.assertFalse(s.matchesrules("haegwjzuvuyypxyu", s.patterns1))
+        self.assertFalse(s.matchesrules("dvszwmarrgswjxmb", s.patterns1))
 
+    def test_nice2(self):
+        self.assertTrue(s.matchesrules("qjhvhtzxzqqjkmpb", s.patterns2))
+        self.assertTrue(s.matchesrules("xxyxx", s.patterns2))
+
+    def test_naughty2(self):
+        self.assertFalse(s.matchesrules("uurcxstgmygtbstg", s.patterns2))
+        self.assertFalse(s.matchesrules("ieodomkazucvgmuy", s.patterns2))
 
 if __name__ == '__main__':
     unittest.main()
--- day5/script.py
+++ day5/script.py
@@ -1,6 +1,6 @@
 import re
 
-patterns = [
+patterns1 = [
     # at least 3 vowels
     re.compile(r".*[aeiou].*[aeiou].*[aeiou].*"),
     # at least one double character
@@ -9,8 +9,15 @@ patterns = [
     re.compile(r"^((?!(?:ab|cd|pq|xy)).)*$")
 ]
 
+patterns2 = [
+    # a pair of letters that does not overlap
+    re.compile(r".*(..).*\1.*"),
+    # one repeating letter with one other letter between them
+    re.compile(r".*(\w)[^\1]\1.*")
+]
 
-def matchesrules(string):
+
+def matchesrules(string, patterns):
     for p in patterns:
         if p.match(string) is None:
             return False
@@ -21,7 +28,7 @@ def adventofcode5():
     counter = 0
     with open("input") as file:
         for string in file:
-            if matchesrules(string):
+            if matchesrules(string, patterns2):
                 counter += 1
     print("Number of nice words: {}".format(counter))
 
